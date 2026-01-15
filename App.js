2e295a66e88ea4f14ff8cf013b0ea274ac94f909
kyle-darren-laguerta <kyledarrendasco.laguerta24@bicol-u.edu.ph>
2026-01-15 23:33:34 +0800

    feat: stack screen navigation setup

--- App.js
+++ App.js
@@ -2,9 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { CameraView, useCameraPermissions } from 'expo-camera';
 import ScannerScreen from './screens/ScannerScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
+
   return (
-    <ScannerScreen />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="ScannerScreen" component={ScannerScreen} options={{ headerShown: false, }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
