5009b94ca05552df058ceb768b4008149504330d
kyle-darren-laguerta <kyledarrendasco.laguerta24@bicol-u.edu.ph>
2026-01-17 09:29:18 +0800

    feat: finished contructing the header component

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,20 @@
 import { View, Text } from "react-native";
+import { useFonts } from 'expo-font';
 
 export default function Header() {
+    const [loaded] = useFonts({
+        'AlphaSlabOne-Regular': require('../assets/fonts/AlfaSlabOne-Regular.ttf'),
+    });
 
     return (
-        <View style={{ height: 100}}>
-            <View style={{ flex: 2, backgroundColor: "#86112e" }}>
-                <Text>L.I.F.E.L.I.N.E</Text>
-                <Text>Life-saving Integrated First-aid and Emergency Link for Immediate Notification in Education </Text>
+        <View style={{ height: 100, marginBottom: 30, }}>
+            <View style={{ flex: 4, backgroundColor: "#86112e", alignItems: "center", justifyContent: "center", }}>
+                <View style={{ width: "60%", alignItems: "center", justifyContent: "center", }}>
+                    <Text style={{ color: "white", fontSize: 30, fontFamily: "AlphaSlabOne-Regular" }}>L.I.F.E.L.I.N.E</Text>
+                    <Text style={{ color: "white", textAlign: "center", fontSize: 9, marginTop: -7, }}>Life-saving Integrated First-aid and Emergency Link for Immediate Notification in Education </Text>
+                </View>
             </View>
-            <View style={{ flex: 3, backgroundColor: "#740f28" }}></View>
+            <View style={{ flex: 1, backgroundColor: "#740f28" }}></View>
         </View>
     );
 }
